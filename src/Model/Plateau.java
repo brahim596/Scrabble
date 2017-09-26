f05e8043d49b7349dcfef9cb757dd45ec3a061d3
brahim596 <brahim.dje.59@gmail.com>
2017-09-26 13:22:35 +0200

    Constructeur plateau

--- src/Model/Plateau.java
+++ src/Model/Plateau.java
@@ -7,6 +7,11 @@ public class Plateau {
 	public Plateau()
 	{
 		
+		
+	}
+	
+	public void Construire_Tableau(){
+		
 		setPlateau(new Case[15][15]);
 		
 		for(int i=0;i<15;i++)
@@ -16,12 +21,54 @@ public class Plateau {
 				
 				if(i==0)
 				{
+					switch (y){
 					
+					case 0: plateau[i][y]=new Case("MT");break;
+					case 3: plateau[i][y]=new Case("LD");break;
+					case 7: plateau[i][y]=new Case("MT");break;
+					case 11: plateau[i][y]=new Case("LD");break;
+					case 14: plateau[i][y]=new Case("MT");break;
+					default:plateau[i][y]=new Case("");break;
+					
+					}
 					
 				}
-				
-				
-				
+				else if(i==1)
+				{
+					switch (y){
+					
+					case 1: plateau[i][y]=new Case("MD");break;
+					case 5: plateau[i][y]=new Case("LT");break;
+					case 9: plateau[i][y]=new Case("LT");break;
+					case 13: plateau[i][y]=new Case("MD");break;
+					default:plateau[i][y]=new Case("");break;
+					
+					}
+				}
+				else if(i==2)
+				{
+					switch (y){
+					
+					case 2: plateau[i][y]=new Case("MD");break;
+					case 6: plateau[i][y]=new Case("LD");break;
+					case 8: plateau[i][y]=new Case("LD");break;
+					case 12: plateau[i][y]=new Case("MD");break;
+					default:plateau[i][y]=new Case("");break;
+					
+					}
+				}
+				else if(i==3)
+				{
+					switch (y){
+					case 0: plateau[i][y]=new Case("LD");break;
+					case 3: plateau[i][y]=new Case("MD");break;
+					case 7: plateau[i][y]=new Case("LD");break;
+					case 11: plateau[i][y]=new Case("MD");break;
+					case 14: plateau[i][y]=new Case("LD");break;
+					default:plateau[i][y]=new Case("");break;
+					
+					}
+				}
 				
 			}
 		}
