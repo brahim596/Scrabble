f1c53a6e60cfe81c4a113a7b832585d7c43f5559
zackaria <zachour59@gmail.com>
2017-09-29 13:31:46 +0200

    lignes du tableau de la fonction construire_tableau

--- src/Model/Plateau.java
+++ src/Model/Plateau.java
@@ -22,7 +22,6 @@ public class Plateau {
 				if(i==0)
 				{
 					switch (y){
-					
 					case 0: plateau[i][y]=new Case("MT");break;
 					case 3: plateau[i][y]=new Case("LD");break;
 					case 7: plateau[i][y]=new Case("MT");break;
@@ -36,7 +35,6 @@ public class Plateau {
 				else if(i==1)
 				{
 					switch (y){
-					
 					case 1: plateau[i][y]=new Case("MD");break;
 					case 5: plateau[i][y]=new Case("LT");break;
 					case 9: plateau[i][y]=new Case("LT");break;
@@ -48,7 +46,6 @@ public class Plateau {
 				else if(i==2)
 				{
 					switch (y){
-					
 					case 2: plateau[i][y]=new Case("MD");break;
 					case 6: plateau[i][y]=new Case("LD");break;
 					case 8: plateau[i][y]=new Case("LD");break;
@@ -69,7 +66,127 @@ public class Plateau {
 					
 					}
 				}
-				
+				else if(i==4)
+				{
+					switch (y){
+					case 4: plateau[i][y]=new Case("MD");break;
+					case 10: plateau[i][y]=new Case("MD");break;
+					default:plateau[i][y]=new Case("");break;
+
+					}
+				}
+				else if(i==5)
+				{
+					switch (y){
+					case 1: plateau[i][y]=new Case("LT");break;
+					case 5: plateau[i][y]=new Case("LT");break;
+					case 9: plateau[i][y]=new Case("LT");break;
+					case 13: plateau[i][y]=new Case("LT");break;
+					default:plateau[i][y]=new Case("");break;
+
+					}
+				}
+				else if(i==6)
+				{
+					switch (y){
+					case 2: plateau[i][y]=new Case("LD");break;
+					case 6: plateau[i][y]=new Case("LD");break;
+					case 8: plateau[i][y]=new Case("LD");break;
+					case 12: plateau[i][y]=new Case("LD");break;
+					default:plateau[i][y]=new Case("");break;
+
+					}
+				}
+				else if(i==7)
+				{
+					switch (y){
+					case 0: plateau[i][y]=new Case("MT");break;
+					case 3: plateau[i][y]=new Case("LD");break;
+					case 7: plateau[i][y]=new Case("ETOILE");break;
+					case 11: plateau[i][y]=new Case("LD");break;
+					case 14: plateau[i][y]=new Case("MT");break;
+					default:plateau[i][y]=new Case("");break;
+
+					}
+				}
+				else if(i==8)
+				{
+					switch (y){
+					case 2: plateau[i][y]=new Case("LD");break;
+					case 6: plateau[i][y]=new Case("LD");break;
+					case 8: plateau[i][y]=new Case("LD");break;
+					case 12: plateau[i][y]=new Case("LD");break;
+					default:plateau[i][y]=new Case("");break;
+
+					}
+				}
+				else if(i==9)
+				{
+					switch (y){
+					case 1: plateau[i][y]=new Case("LT");break;
+					case 5: plateau[i][y]=new Case("LT");break;
+					case 9: plateau[i][y]=new Case("LT");break;
+					case 13: plateau[i][y]=new Case("LT");break;
+					default:plateau[i][y]=new Case("");break;
+
+					}
+				}
+				else if(i==10)
+				{
+					switch (y){
+					case 4: plateau[i][y]=new Case("MD");break;
+					case 10: plateau[i][y]=new Case("MD");break;
+					default:plateau[i][y]=new Case("");break;
+
+					}
+				}
+				else if(i==11)
+				{
+					switch (y){
+					case 0: plateau[i][y]=new Case("LD");break;
+					case 3: plateau[i][y]=new Case("MD");break;
+					case 7: plateau[i][y]=new Case("LD");break;
+					case 11: plateau[i][y]=new Case("MD");break;
+					case 14: plateau[i][y]=new Case("LD");break;
+					default:plateau[i][y]=new Case("");break;
+					
+					}
+				}
+				else if(i==12)
+				{
+					switch (y){
+					case 2: plateau[i][y]=new Case("MD");break;
+					case 6: plateau[i][y]=new Case("LD");break;
+					case 8: plateau[i][y]=new Case("LD");break;
+					case 12: plateau[i][y]=new Case("MD");break;
+					default:plateau[i][y]=new Case("");break;
+					
+					}
+				}
+				else if(i==13)
+				{
+					switch (y){
+					case 1: plateau[i][y]=new Case("MD");break;
+					case 5: plateau[i][y]=new Case("LT");break;
+					case 9: plateau[i][y]=new Case("LT");break;
+					case 13: plateau[i][y]=new Case("MD");break;
+					default:plateau[i][y]=new Case("");break;
+					
+					}
+				}
+				else if(i==14)
+				{
+					switch (y){
+					case 0: plateau[i][y]=new Case("MT");break;
+					case 3: plateau[i][y]=new Case("LD");break;
+					case 7: plateau[i][y]=new Case("MT");break;
+					case 11: plateau[i][y]=new Case("LD");break;
+					case 14: plateau[i][y]=new Case("MT");break;
+					default:plateau[i][y]=new Case("");break;
+					
+					}
+				}
+			
 			}
 		}
 	}
