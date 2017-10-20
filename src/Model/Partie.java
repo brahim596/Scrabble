2fc258ff7118a721959ec19cdbdd2c6a7e952328
zackaria <zachour59@gmail.com>
2017-10-20 10:30:15 +0200

    debugZz

--- src/Model/Partie.java
+++ src/Model/Partie.java
@@ -8,40 +8,36 @@ public class Partie {
 	private Sac sac;
 	private Plateau plateau;
 	private Dictionnaire dico;
-	
-	public Partie(ArrayList<Joueur> joueurs,Sac sac,Plateau plateau)
-	{
-		this.joueurs=joueurs;
-		this.sac=sac;
-		this.plateau=plateau;
-		
+
+	public Partie(ArrayList<Joueur> joueurs, Sac sac, Plateau plateau) {
+		this.joueurs = joueurs;
+		this.sac = sac;
+		this.plateau = plateau;
+
 	}
-	
-	public Joueur designePremierJoueur()
-	{
-		
-		for (Joueur j: joueurs)
-		{
+
+	public Joueur designePremierJoueur() {
+
+
+		for (Joueur j : joueurs) {
 			j.piocher(sac);
 		}
-		
+
 		return null;
-		
-		
+
 	}
-	
-	public static void main(String[] args)
-	{
-		Sac sac=new Sac();
-		Joueur j1=new Joueur(1,"Cr7",0);
-		Joueur j2=new Joueur(2,"Messi",0);
-		
-		ArrayList<Joueur> joueurs=new ArrayList<Joueur>();
+
+	public static void main(String[] args) {
+		Sac sac = new Sac();
+		Joueur j1 = new Joueur(1, "Cr7", 0);
+		Joueur j2 = new Joueur(2, "Messi", 0);
+
+		ArrayList<Joueur> joueurs = new ArrayList<Joueur>();
 		joueurs.add(j1);
 		joueurs.add(j2);
-		
-		Plateau plateau=new Plateau();
-		
-		Partie p=new Partie(joueurs,sac,plateau);
+
+		Plateau plateau = new Plateau();
+
+		Partie p = new Partie(joueurs, sac, plateau);
 	}
 }
