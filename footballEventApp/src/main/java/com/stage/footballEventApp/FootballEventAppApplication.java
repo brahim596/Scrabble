fdaf8810f20874066d48b2a9177352061032b597
brahim596 <brahim.dje.59@gmail.com>
2018-08-02 23:12:25 +0200

    maj

--- footballEventApp/src/main/java/com/stage/footballEventApp/FootballEventAppApplication.java
+++ footballEventApp/src/main/java/com/stage/footballEventApp/FootballEventAppApplication.java
@@ -2,13 +2,20 @@ package com.stage.footballEventApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "./")
-public class FootballEventAppApplication {
+@ComponentScan(basePackages = {"Controller","Model","Service"})
+public class FootballEventAppApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FootballEventAppApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(FootballEventAppApplication.class);
 	}
 }
